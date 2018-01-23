0c2e25be0408ab2d2e9de520930e93d5c6b2a758
Pascal Schumacher <pascalschumacher@gmx.net>
2018-01-23 22:44:05 +0100

    initial commit

---
+++ src/test/java/test/EventNotifierExchangeSentExampleTest.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package test;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringRunner;
+import org.apache.camel.test.spring.CamelTestContextBootstrapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(CamelSpringRunner.class)
+@BootstrapWith(CamelTestContextBootstrapper.class)
+@ContextConfiguration(locations = {"classpath:/test-beans.xml"})
+public class EventNotifierExchangeSentExampleTest {
+
+    @EndpointInject(uri = "direct:start")
+    ProducerTemplate producer;
+    @EndpointInject(uri = "mock:result")
+    MockEndpoint out;
+
+    @Test
+    public void testExchangeSent() throws Exception {
+        out.expectedMessageCount(1);
+
+        producer.sendBody("Hello World");
+
+        out.assertIsSatisfied();
+    }
+}
